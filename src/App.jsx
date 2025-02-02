1079e31dab7d2ba7b087cc53e87eeb4822a978a1
Farhan Siddiqui <farhansiddiqui2333@gmail.com>
2025-02-02 10:33:22 +0500

    Create Card Element

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Card from './components/Card'
+import Img1 from './assets/img1.jpg'
+import Img2 from './assets/img2.jpg'
+import Img3 from './assets/img3.jpg'
+import Img4 from './assets/img4.webp'
+import Img5 from './assets/img5.jpg'
+
+function App() {
+  return (
+    <>
+      <div className='flex gap-10 flex-wrap w-full h-screen p-10 justify-center items-center'>
+        <Card profilePhoto={Img1} name='Xtylish@nsari' username='Ansari720@' />
+        <Card profilePhoto={Img2} name='Jeff Smith' username='jeff9341' />
+        <Card profilePhoto={Img3} name='Simran Hez' username='ssimranh@z' />
+        <Card profilePhoto={Img4} name='ShamsiOP' username='@shamsi444' />
+        <Card profilePhoto={Img5} name='Legend Killer' username='unknownOP' />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Card = (props) => {
+    return (
+        <div className='bg-white w-80 h-56 rounded-xl shadow-md shadow-white p-1'>
+            <div className="img h-1/2 w-full rounded-xl px-2 py-1">
+                <i class="fa-brands fa-instagram text-3xl text-white"></i>
+                <img src={props.profilePhoto} alt="" className='h-24 w-24 rounded-full relative left-1/2 translate-y-2 -translate-x-1/2 border-2 border-white border-solid' />
+            </div>
+            <div className="content text-center pt-5 relative">
+                <h1 className='text-slate-900 font-extrabold text-2xl mt-5'>{props.name}</h1>
+                <p className='text-sm text-slate-700'>{props.username}</p>
+                <button className='btn absolute right-1 bottom-0 translate-y-3 w-8 h-8 text-white rounded-md'><i class="fa-solid fa-plus"></i></button>
+            </div>
+        </div>
+    )
+}
+
+export default Card
