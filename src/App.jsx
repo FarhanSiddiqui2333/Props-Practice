5ba2f6859e7c7c53d0d3b72d103375accd70f400
Farhan Siddiqui <farhansiddiqui2333@gmail.com>
2025-02-02 10:40:05 +0500

    Pass Props in the Form of String

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,11 @@ function App() {
   return (
     <>
       <div className='flex gap-10 flex-wrap w-full h-screen p-10 justify-center items-center'>
-        <Card profilePhoto={Img1} name='Xtylish@nsari' username='Ansari720@' />
-        <Card profilePhoto={Img2} name='Jeff Smith' username='jeff9341' />
-        <Card profilePhoto={Img3} name='Simran Hez' username='ssimranh@z' />
-        <Card profilePhoto={Img4} name='ShamsiOP' username='@shamsi444' />
-        <Card profilePhoto={Img5} name='Legend Killer' username='unknownOP' />
+        <Card profilePhoto={Img1} name='Xtylish@nsari' username='Ansari720@'/>
+        <Card profilePhoto={Img2} name='Jeff Smith' username='jeff9341'/>
+        <Card profilePhoto={Img3} name='Simran Hez' username='ssimranh@z'/>
+        <Card profilePhoto={Img4} name='ShamsiOP' username='@shamsi444'/>
+        <Card profilePhoto={Img5} name='Legend Killer' username='unknownOP'/>
       </div>
     </>
   )
